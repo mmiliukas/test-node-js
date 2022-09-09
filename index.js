2fd1f38fcf2d9b045249323f5a4315b5f79787db
mmiliukas <56244003+mmiliukas@users.noreply.github.com>
2022-09-09 18:09:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
+  .get('/a/b/c/d', (req, res) => {
+     res.redirect('../../');
+  })
   .get('*', (req, res) => {
     const h1 = JSON.stringify(req.headers, null, 2);
     const h2 = JSON.stringify(req.rawHeaders, null, 2);
