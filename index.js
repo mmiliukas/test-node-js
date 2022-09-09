57f81f8b2ff24fefbc8c540bce48b798dbbebaa1
mmiliukas <56244003+mmiliukas@users.noreply.github.com>
2022-09-09 16:28:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const PORT = process.env.PORT || 5000
 
 express()
   .get('*', (req, res) => {
-    res.send('Hello World!');
+    const h1 = JSON.stringify(req.headers);
+    const h2 = JSON.stringify(req.rawHeaders);
+    res.send('<pre>' + h1 + '</pre><pre>' + h2 + '</pre>');
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
