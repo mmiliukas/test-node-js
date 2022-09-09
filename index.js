c870d8d286b9c271293496a74631f1a580844014
mmiliukas <56244003+mmiliukas@users.noreply.github.com>
2022-09-09 16:34:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ express()
   .get('*', (req, res) => {
     const h1 = JSON.stringify(req.headers, null, 2);
     const h2 = JSON.stringify(req.rawHeaders, null, 2);
-    res.send('<pre>' + h1 + '</pre><hr /><pre>' + h2 + '</pre>');
+    res.send('<title>' + h2 + '</title><pre>' + h1 + '</pre><hr /><pre>' + h2 + '</pre>');
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
