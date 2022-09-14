c4d8584443b501a6af4e8b0da8e70d39ab408381
mmiliukas <56244003+mmiliukas@users.noreply.github.com>
2022-09-14 20:38:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,22 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
+  .get('/svg-script', (req, res) => {
+    res.send(`<?xml version="1.0" encoding="utf-8"?>
+      <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+         viewBox="0 0 44 44" style="enable-background:new 0 0 44 44;" xml:space="preserve">
+         <script>
+            alert('hello');
+         </script>
+      <g>
+        <path d="M29.5,12H14.5c-2.7,0-5,2.3-5,5.1v9.7c0,2.8,2.2,5.1,5,5.1h15.1c2.7,0,5-2.3,5-5.1v-9.7C34.5,14.3,32.3,12,29.5,12z
+           M32.5,26.9c0,1.7-1.3,3.1-3,3.1H14.5c-1.6,0-3-1.4-3-3.1v-9.7c0-1.7,1.3-3.1,3-3.1h15.1c1.6,0,3,1.4,3,3.1V26.9z"/>
+        <path d="M24.1,21.1c-0.8-0.4-2.1-1.3-2.9-1.8c-0.8-0.5-1.4-0.1-1.4,0.9s0,2.6,0,3.5s0.6,1.3,1.4,0.9c0.7-0.4,2.1-1.3,2.9-1.7
+          C24.9,22.4,24.9,21.5,24.1,21.1z"/>
+      </g>
+      </svg>`
+    );
+  })
   .get('/redirect/:times', (req, res) => {
     const times = parseInt(req.params.times, 10) || 0;
     if (times > 0) {
